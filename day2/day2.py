a884918ee8123546a835ed37e600d715374ac964
Geoffrey Hunter <gbmhunter@gmail.com>
2017-12-04 21:20:03 -0800

    Added working code for day 2 part 2.

--- day2/day2.py
+++ day2/day2.py
@@ -1,11 +1,23 @@
 import csv
 
 sum = 0
+part2Sum = 0
 with open('input.txt', newline='') as csvfile:
     csvReader = csv.reader(csvfile, delimiter='\t', quotechar='|')
     for row in csvReader:
         rowAsInt = [int(numeric_string) for numeric_string in row]
         diff = max(rowAsInt) - min(rowAsInt)        
-        sum += diff
+        sum += diff        
 
-print('output = ' + str(sum))
+        for index1, firstValue in enumerate(rowAsInt):
+            for index2, secondValue in enumerate(rowAsInt):
+                if index1 == index2:
+                    continue
+            
+                modulus = firstValue % secondValue                
+                if modulus == 0:                    
+                    part2Sum += firstValue / secondValue                            
+
+
+print('part 1 answer = ' + str(sum))
+print('part 2 answer = ' + str(part2Sum))
